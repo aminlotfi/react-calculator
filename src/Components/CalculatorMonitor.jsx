928dbe295025ec5097a19b254383e1f2c551edeb
Mohammad Amin Lotfi <a.lotfi1201@gmail.com>
2023-04-12 20:56:37 +0330

    Logic added

--- src/Components/CalculatorMonitor.jsx
+++ src/Components/CalculatorMonitor.jsx
@@ -1,6 +1,6 @@
 const CalculatorMonitor = ({ currentOperand, prevOperand, operation }) => {
     return (
-        <div className="flex flex-col items-end justify-between w-full px-4 py-5 rounded-md mb-4">
+        <div className="flex flex-col items-end justify-between w-full px-4 py-5 rounded-md mb-4 min-h-[112px]">
             <div className="text-md mb-3">{prevOperand} {operation}</div>
             <div className="text-3xl whitespace-pre-wrap break-all">{currentOperand}</div>
         </div>
--- src/Templates/Calculator/Calculator.jsx
+++ src/Templates/Calculator/Calculator.jsx
@@ -11,40 +11,128 @@ export const ACTIONS = {
     DELETE_NUMBER: 'delete-number',
 }
 
-const reducer = (state, action) => {
-    switch (action.type) {
+const reducer = (state, {type, payload}) => {
+    switch (type) {
         case ACTIONS.SELECT_NUMBER:
+            if (state.overwrite) {
+                return {
+                    ...state,
+                    overwrite: false,
+                    currentOperand: payload.number
+                }
+            }
+            if (payload.number === '.' && state.currentOperand.includes('.')) return state;
+            if (state.currentOperand === '0' && payload.number === '0') return state;
             return {
                 ...state,
-                currentOperand: `${state.currentOperand || '0'}${action.payload}`,
+                currentOperand: `${state.currentOperand || ''}${payload.number}`,
             }
+        case ACTIONS.SELECT_OPERATOR:
+            if (state.currentOperand == null) {
+                return {
+                    ...state,
+                    operation: payload.operation
+                }
+            }
+            if (state.prevOperand != null) {
+                return {
+                    ...state,
+                    prevOperand: calculate(state),
+                    operation: payload.operation,
+                    currentOperand: null
+                }
+            }
+            return {
+                ...state,
+                prevOperand: state.currentOperand,
+                operation: payload.operation,
+                currentOperand: null
+            }
+        case ACTIONS.DELETE_NUMBER:
+            if (state.overwrite) {
+                return {
+                    ...state,
+                    overwrite: false,
+                    currentOperand: null
+                }
+            }
+            if (state.currentOperand == null) return state
+            return {
+                ...state,
+                currentOperand: state.currentOperand.slice(0, -1)
+            }
+        case ACTIONS.CLEAR:
+            return {}
+        case ACTIONS.CALCULATE:
+            if (state.currentOperand == null || state.prevOperand == null || state.operation == null) return state
+            return {
+                ...state,
+                overwrite: true,
+                prevOperand: null,
+                operation: null,
+                currentOperand: calculate(state)
+            }
+
     }
 }
 
-const Calculator = () => {
-    const [{ currentOperand, prevOperand, operation }, dispatch] = useReducer(reducer, {});
+const calculate = ({currentOperand, prevOperand, operation}) => {
+    const current = +currentOperand
+    const previous = +prevOperand
+    let result = ''
+    switch (operation) {
+        case '/':
+            result = previous / current
+            break
+        case '*':
+            result = previous * current
+            break
+        case '-':
+            result = previous - current
+            break
+        case '+':
+            result = previous + current
+            break
+    }
+    return result.toString()
+}
 
-    return <div className="flex flex-col items-center justify-start p-4 bg-white rounded-xl w-full max-w-[500px]">
+const Calculator = () => {
+    const [{currentOperand, prevOperand, operation}, dispatch] = useReducer(reducer, {});
+
+    return <div className="flex flex-col items-center justify-start p-4 bg-white rounded-xl w-full max-w-[500px] select-none">
         <CalculatorMonitor currentOperand={currentOperand} prevOperand={prevOperand} operation={operation}/>
         <div className="grid grid-cols-4 gap-4 w-full">
-            <button className="col-span-2 flex item-center justify-center bg-gray-300 hover:bg-gray-400 p-4 rounded-[10px] text-xl">AC</button>
-            <button className="flex item-center justify-center bg-gray-300 hover:bg-gray-400 p-4 rounded-[10px] text-xl">C</button>
-            <OperationButton operation={'/'} dispatch={dispatch} />
+            <button
+                onClick={() => dispatch({type: ACTIONS.CLEAR})}
+                className="col-span-2 flex item-center justify-center bg-gray-300 hover:bg-gray-400 p-4 rounded-[10px] text-xl">
+                AC
+            </button>
+            <button
+                onClick={() => dispatch({type: ACTIONS.DELETE_NUMBER})}
+                className="flex item-center justify-center bg-gray-300 hover:bg-gray-400 p-4 rounded-[10px] text-xl">
+                C
+            </button>
+            <OperationButton operation={'/'} dispatch={dispatch}/>
             <NumberButton number={7} dispatch={dispatch}/>
             <NumberButton number={8} dispatch={dispatch}/>
             <NumberButton number={9} dispatch={dispatch}/>
-            <OperationButton operation={'*'} dispatch={dispatch} />
+            <OperationButton operation={'*'} dispatch={dispatch}/>
             <NumberButton number={4} dispatch={dispatch}/>
             <NumberButton number={5} dispatch={dispatch}/>
             <NumberButton number={6} dispatch={dispatch}/>
-            <OperationButton operation={'-'} dispatch={dispatch} />
+            <OperationButton operation={'-'} dispatch={dispatch}/>
             <NumberButton number={1} dispatch={dispatch}/>
             <NumberButton number={2} dispatch={dispatch}/>
             <NumberButton number={3} dispatch={dispatch}/>
-            <OperationButton operation={'+'} dispatch={dispatch} />
+            <OperationButton operation={'+'} dispatch={dispatch}/>
             <NumberButton number={0} dispatch={dispatch}/>
             <NumberButton number={'.'} dispatch={dispatch}/>
-            <button className="col-span-2 flex item-center justify-center bg-green-500 hover:bg-green-600 p-4 rounded-[10px] text-xl">=</button>
+            <button
+                onClick={() => dispatch({type: ACTIONS.CALCULATE})}
+                className="col-span-2 flex item-center justify-center bg-green-500 hover:bg-green-600 p-4 rounded-[10px] text-xl">
+                =
+            </button>
         </div>
     </div>;
 }
